54706a6c1142b244dff6f8e67e9d7561dd783fa5
adhy-k <adhykrishna123@gmail.com>
2025-07-15 11:44:08 +0530

    initial commit

---
+++ src/components/Adduser.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+
+const Adduser = () => {
+  return (
+    <div>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Id</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Node Id</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Html URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Followers URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Following URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Avathar URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Gravatar URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Html URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Followers URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Gists URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Starred URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Subscriptions URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Organizations URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Repos URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Events URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Received Events URL</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Type</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">User view Type</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <div className="button btn btn-primary">Submit</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Adduser
