e7b00dc525cde5aa14887b4ad014308f9682f79e
adhy-k <adhykrishna123@gmail.com>
2025-07-15 12:15:59 +0530

    updated view users

--- src/components/Searchuser.jsx
+++ src/components/Searchuser.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Searchuser = () => {
-  return (
-    <div>
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    
+    const[input,changeInput]=useState(
+        {Id:""}
+    )
+    const inputHandler=(event)=>{
+        changeInput({...input,[event.target.name]:event.target.value})
+    }
+    const readInput=()=>{
+        console.log(input)
+    }
+    return (
+        <div>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Id</label>
+                                <input type="text" className="form-control" name='Id' value={input.Id} onChange={inputHandler} />
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <div className="button btn btn-primary" onClick={readInput}>Submit</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        </div>
-  )
+    )
 }
 
 export default Searchuser
--- src/components/Viewusers.jsx
+++ src/components/Viewusers.jsx
@@ -1,17 +1,79 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Viewusers = () => {
-  return (
-    <div>
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    
+    const [user, changeUser] = useState([])
+    const fetchdata = () => {
+        axios.get("https://api.github.com/users").then(
+            (req) => {
+                changeUser(req)
+            }
+        ).catch()
+    }
+    useEffect(() => { fetchdata() }, [])
+    return (
+        <div>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Id</th>
+                                    <th scope="col">Node Id</th>
+                                    <th scope="col">Avatar UR</th>
+                                    <th scope="col">Gravatar UR</th>
+                                    <th scope="col">URL</th>
+                                    <th scope="col">Html URL</th>
+                                    <th scope="col">Followers URL</th>
+                                    <th scope="col">Following URL</th>
+                                    <th scope="col">Gists UR</th>
+                                    <th scope="col">Starred URL</th>
+                                    <th scope="col">Subscriptions URL</th>
+                                    <th scope="col">Organizations URL</th>
+                                    <th scope="col">Repos URL</th>
+                                    <th scope="col">Events URL</th>
+                                    <th scope="col">Received Events URL</th>
+                                    <th scope="col">Type</th>
+                                    <th scope="col">User View Type</th>
+                                </tr>
+                            </thead>
+                            {
+                                user.map(
+                                    (value, index) => {
+                                        return (
+                                            <tbody>
+                                                <tr>
+                                                    <th scope="row">{value.id}</th>
+                                                    <td>{value.node_id}</td>
+                                                    <td>{value.avatar_url}</td>
+                                                    <td>{value.gravatar_id}</td>
+                                                    <td>{value.urlt}</td>
+                                                    <td>{value.html_url}</td>
+                                                    <td>{value.followers_url}</td>
+                                                    <td>{value.following_url}</td>
+                                                    <td>{value.gists_url}</td>
+                                                    <td>{value.starred_url}</td>
+                                                    <td>{value.subscriptions_url}</td>
+                                                    <td>{value.organizations_url}</td>
+                                                    <td>{value.repos_url}</td>
+                                                    <td>{value.events_url}</td>
+                                                    <td>{value.received_events_url}</td>
+                                                    <td>{value.type}</td>
+                                                    <td>{value.user_view_type}</td>
+                                                </tr>
+
+                                            </tbody>
+                                        )
+                                    }
+                                )
+                            }
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Viewusers
